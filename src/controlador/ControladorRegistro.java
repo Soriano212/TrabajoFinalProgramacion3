45c47fd358a495201f2e6a26d52285e61a59d75b
Alberto Soriano <albertosoriano.e@gmail.com>
2021-07-17 16:04:16 -0500

    Cambios en ControladorRegistro
    Co-authored-by: juanjamalovega@hotmail.com <juanjamalovega@hotmail.com>

--- src/controlador/ControladorRegistro.java
+++ src/controlador/ControladorRegistro.java
@@ -1,8 +1,12 @@
 package controlador;
 
 import vista.Registro;
+import vista.VentanaListaPersonas;
+
 import java.awt.event.*;
 import java.time.*;
+
+import javax.swing.JOptionPane;
 
 import modelo.ManejoHuespedes;
 
@@ -10,10 +14,12 @@ public class ControladorRegistro implements ActionListener{
 
     private Registro vista;
     private ManejoHuespedes manejo;
+    private VentanaListaPersonas vistaLista;
 
     public ControladorRegistro(Registro vista){
         this.vista = vista;
         this.manejo = ManejoHuespedes.getListas();
+        this.vistaLista = new VentanaListaPersonas();
 
         //Pone a la escucha a los botones
         vista.btnRegistrar.addActionListener(this);
@@ -63,44 +69,79 @@ public class ControladorRegistro implements ActionListener{
 
         int mesban = Integer.parseInt(vista.textFieldMesBan.getText());
         int anioban = Integer.parseInt(vista.textFieldAnioBan.getText());
-        LocalDate fecCad = LocalDate.of(anioban,mesban,0);
+        LocalDate fecCad = LocalDate.of(anioban,mesban,1);
+
+        boolean afil;
+        if(vista.rdbtnSi.isSelected()){
+            afil = true;
+        }
+        else{
+            afil = false;
+        }
+
+        LocalDate fecAf;
+        if(afil){
+            fecAf = LocalDate.now();
+        }else{
+            fecAf = null;
+        }
 
         if(agregar){
             manejo.addValor(iden, nom, ape, fecNac, cell, fijo, dir, prof, banco, numtar, fecCad, afil, fecAf);
+            return true;
+        }
+        else{
+            if(manejo.modificarHuesped(iden, nom, ape, fecNac, cell, fijo, dir, prof, banco, numtar, fecCad, afil)){
+                return true;
+            }
         }
 
+        return false;
+    }
+
+    public void limpiar(){
+        vista.textFieldIden.setText(null);
+        vista.textFieldNombres.setText(null);
+        vista.textFieldApellidos.setText(null);
+        vista.textFieldProf.setText(null);
+        vista.textFieldCell.setText(null);+
+        vista.textFieldFijo.setText(null);
+        vista.textFieldDirec.setText(null);
+        vista.textFieldBanco.setText(null);
+        vista.textFieldNumTar.setText(null);
+        
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         
         if(e.getSource()== vista.btnRegistrar){
-            
-            
+            if(agregarModificar(true)){
+                JOptionPane.showMessageDialog(null, "Huesped agregado.");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Error al agregar, verifique los campos de texto.");
+            }
         }
+
         if(e.getSource()== vista.btnVer){
 
-
-
-
         }
+
         if(e.getSource()== vista.btnLimpiar){
-            
-
-
 
         }
+
         if(e.getSource()== vista.btnModificar){
-            
-
-
-
+            if(agregarModificar(false)){
+                JOptionPane.showMessageDialog(null, "Huesped modificado.");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Error al modificar, verifique los campos de texto.");
+            }
         }
         if(e.getSource()== vista.btnListar){
             
-
-
-
         }
         
     }
