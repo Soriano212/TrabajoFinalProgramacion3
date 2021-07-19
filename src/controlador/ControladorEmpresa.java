b453c855d59c5a94c90835e176f62b62c22f78f7
Alberto Soriano <albertosoriano.e@gmail.com>
2021-07-18 21:40:10 -0500

    Cambios en Controladores y ventan Empresa

--- src/controlador/ControladorEmpresa.java
+++ src/controlador/ControladorEmpresa.java
@@ -1,9 +1,24 @@
 package controlador;
 
+import javax.swing.table.DefaultTableModel;
+
+import modelo.Empresa;
 import vista.VistaEmpresa;
 
 public class ControladorEmpresa extends AdaptadorListener{
     
     private VistaEmpresa vista;
+    private Empresa manejo;
+    private DefaultTableModel modelTabla;
+    private ControladorTeclado soloNumeros;
+    private ControladorTeclado soloIden;
+
+    public ControladorEmpresa(VistaEmpresa vista){
+        this.vista = vista;
+        this.manejo = Empresa.getEmpresa();
+
+        //Deshabilitamos valores
+
+    }
 
 }
--- src/vista/VistaEmpresa.java
+++ src/vista/VistaEmpresa.java
@@ -13,14 +13,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class VistaEmpresa extends JPanel {
-	private JTextField textIden;
-	private JTextField textNombres;
-	private JTextField textApellidos;
-	private JTable tablePropietarios;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textIden2;
-	private JTextField textProcentaje;
+	public JTextField textIden;
+	public JTextField textNombres;
+	public JTextField textApellidos;
+	public JTextField textNombreEmpresa;
+	public JTextField textDireccionEmpresa;
+	public JTextField textIden2;
+	public JTextField textProcentaje;
+
+	public JLabel lblPorcentaje;
+
+	public JButton btnRegistrar;
+	public JButton btnModificarProp;
+	public JButton btnLimpiar;
+	public JButton btnModificarEmpresa;
+	public JButton btnMas;
+	public JButton btnEliminar;
+	public JButton btnVer;
+
+	public JTable tablePropietarios;
+
+	public JComboBox<String> comboBoxIden;
 
 	/**
 	 * Create the panel.
@@ -47,8 +60,8 @@ public class VistaEmpresa extends JPanel {
 		lblNewLabel_1.setBounds(70, 54, 135, 32);
 		add(lblNewLabel_1);
 		
-		JComboBox comboBoxIden = new JComboBox();
-		comboBoxIden.setModel(new DefaultComboBoxModel(new String[] {"Cedula", "Pasaporte"}));
+		comboBoxIden = new JComboBox<String>();
+		comboBoxIden.setModel(new DefaultComboBoxModel<String>(new String[] {"Cedula", "Pasaporte"}));
 		comboBoxIden.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
 		comboBoxIden.setBounds(279, 59, 115, 24);
 		add(comboBoxIden);
@@ -59,7 +72,7 @@ public class VistaEmpresa extends JPanel {
 		textIden.setBounds(404, 59, 109, 24);
 		add(textIden);
 		
-		JButton btnVer = new JButton("Ver");
+		btnVer = new JButton("Ver");
 		btnVer.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
 		btnVer.setBounds(523, 60, 82, 23);
 		add(btnVer);
@@ -86,19 +99,19 @@ public class VistaEmpresa extends JPanel {
 		textApellidos.setBounds(279, 144, 326, 24);
 		add(textApellidos);
 		
-		JButton btnRegistrar = new JButton("Registrar");
+		btnRegistrar = new JButton("Registrar");
 		btnRegistrar.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-		btnRegistrar.setBounds(70, 285, 153, 23);
+		btnRegistrar.setBounds(70, 285, 109, 23);
 		add(btnRegistrar);
 		
-		JButton btnModificarProp = new JButton("Modificar");
+		btnModificarProp = new JButton("Modificar");
 		btnModificarProp.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-		btnModificarProp.setBounds(264, 285, 153, 23);
+		btnModificarProp.setBounds(217, 285, 109, 23);
 		add(btnModificarProp);
 		
-		JButton btnLimpiar = new JButton("Limpiar");
+		btnLimpiar = new JButton("Limpiar");
 		btnLimpiar.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-		btnLimpiar.setBounds(452, 282, 153, 23);
+		btnLimpiar.setBounds(496, 285, 109, 23);
 		add(btnLimpiar);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -123,11 +136,11 @@ public class VistaEmpresa extends JPanel {
 		lblReservarCabaa_1_1.setBounds(10, 453, 659, 32);
 		add(lblReservarCabaa_1_1);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-		textField.setColumns(10);
-		textField.setBounds(279, 514, 326, 24);
-		add(textField);
+		textNombreEmpresa = new JTextField();
+		textNombreEmpresa.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+		textNombreEmpresa.setColumns(10);
+		textNombreEmpresa.setBounds(279, 514, 326, 24);
+		add(textNombreEmpresa);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Nombre:");
 		lblNewLabel_1_1_1.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 18));
@@ -139,13 +152,13 @@ public class VistaEmpresa extends JPanel {
 		lblNewLabel_1_2_1.setBounds(70, 551, 135, 32);
 		add(lblNewLabel_1_2_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
-		textField_1.setColumns(10);
-		textField_1.setBounds(279, 556, 326, 24);
-		add(textField_1);
+		textDireccionEmpresa = new JTextField();
+		textDireccionEmpresa.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+		textDireccionEmpresa.setColumns(10);
+		textDireccionEmpresa.setBounds(279, 556, 326, 24);
+		add(textDireccionEmpresa);
 		
-		JButton btnModificarEmpresa = new JButton("Modificar");
+		btnModificarEmpresa = new JButton("Modificar");
 		btnModificarEmpresa.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
 		btnModificarEmpresa.setBounds(264, 601, 153, 23);
 		add(btnModificarEmpresa);
@@ -177,16 +190,21 @@ public class VistaEmpresa extends JPanel {
 		lblNewLabel_1_3_1_1.setBounds(535, 225, 21, 32);
 		add(lblNewLabel_1_3_1_1);
 		
-		JButton btnMas = new JButton("+");
+		btnMas = new JButton("+");
 		btnMas.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
 		btnMas.setBounds(533, 188, 72, 23);
 		add(btnMas);
 		
-		JLabel lblPorcentaje = new JLabel("0%");
+		lblPorcentaje = new JLabel("0%");
 		lblPorcentaje.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPorcentaje.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 18));
 		lblPorcentaje.setBounds(343, 182, 135, 32);
 		add(lblPorcentaje);
+		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 16));
+		btnEliminar.setBounds(355, 285, 109, 23);
+		add(btnEliminar);
 
 	}
 }
